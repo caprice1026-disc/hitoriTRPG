8c58b5e3f94c72c9ee50327c6912871756e92beb
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2024-03-06 22:12:21 +0900

    データモデルの変更

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,7 +4,6 @@ import enum
 
 db = SQLAlchemy()
 
-# ゲームの状態を定義するEnum
 class GameStateEnum(enum.Enum):
     PROGRESSING = '進行中'
     GAME_OVER = 'ゲームオーバー'
@@ -37,28 +36,41 @@ class Player(db.Model):
     job = db.Column(db.String(100), nullable=False)
     status = db.Column(JSON, default=lambda: {
         "STR": 0,
-        "DEX": 0,
-        "INT": 0,
-        "AGI": 0,
+        "DEX": 0, 
+        "INT": 0, 
+        "AGI": 0, 
         "LUCK": 0,
-        "HP": 50,  # 後で適切な値に更新する
+        "HP": 50, 
         "SAN": 100
     })
+    conditions = db.Column(JSON, default=lambda: {})  
+    '''
+    コンディコンディションの例
+    {
+    "毒": "毒の状態です。HPが徐々に減少します。",
+    "麻痺": "行動できない状態です。",
+    }
+    '''
+    inventory = db.Column(JSON, default=lambda: {})  # インベントリを保存するフィールド
+    '''inventoryの例
+    {
+    "短剣": "攻撃力+5の武器です。",
+    "回復薬": "HPを30回復します。",
+    "鍵": "宝箱を開けるのに必要なアイテムです。"}
+    '''
     world_id = db.Column(db.Integer, db.ForeignKey('world_setting.id'), nullable=True)
     world = db.relationship('WorldSetting', backref='players', uselist=False)
-    game_state = db.relationship('GameState', backref='player', uselist=False)
 
-class GameState(db.Model):
+class GameSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.Enum(GameStateEnum), nullable=False)
+    # ここはnullの可能性があるので修正するかも
     progress_log = db.Column(db.Text, nullable=False)
-    inventory = db.Column(JSON, nullable=False)
     player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
-    player = db.relationship('Player', backref='game_state', uselist=False)
+    player = db.relationship('Player', backref='game_session', uselist=False)
 
-# 必要に応じて、パスワードのハッシュ化と検証のための実際の関数を実装してください。
-# ここで定義したダミーの関数`set_password`と`check_password`は実際のアプリケーションでは適切な実装が必要です。
+# 必要に応じて、パスワードのハッシュ化と検証のための実際の関数を実装
 
 # データベース初期化のための関数や、プレイヤーのHPとSANを更新するためのロジックは、
 # このファイル外で定義する必要があります。それらはアプリケーションのビジネスロジックの一部として、
-# モデルのインスタンスが作成または更新される際に適切に呼び出されるべきです。
+# モデルのインスタンスが作成または更新される際に適切に呼び出す。
